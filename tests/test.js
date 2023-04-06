63aeb8366ecc866d7f4d21d4250a63c74278d1d0
Stella Andreozzi <stellaandreozzi@Stellas-MacBook-Pro.local>
2023-04-06 11:55:15 -0300

    refactor programm to test

--- tests/test.js
+++ tests/test.js
@@ -4,26 +4,60 @@ const OpenweathermapAPI = require('../api/weather-api')
 require('chromedriver');
 const googleScreen = require('../page_object/google-screen');
 
-async function compareTemperatureTest() {
-   //navigate the app
-    await GoogleSearchPage.goToURL();
+describe('Test: Compare temperature between Google search and WeatherAPI', function(){
+    
+    before(async () => {
+        await GoogleSearchPage.goToURL();
+    });
 
-    //add a todo
-    await GoogleSearchPage.runSearch("Weather in San Francisco, California");
+    it('Should run search on google and find the temperature', async () =>{
+        await GoogleSearchPage.runSearch("Weather in San Francisco, California");
 
-    var title = await driver.getTitle();
-    console.log('Title is:',title);
+        var title = await driver.getTitle();
+        console.log('Title is:',title);
+    
+        const googleTemp = await GoogleSearchPage.getTemp();
+        console.log('Google Temperature:', googleTemp);
+    
+        await googleScreen.closeBrowser();
 
-    const googleTemp = await GoogleSearchPage.getTemp();
-    console.log('Google Temperature:', googleTemp);
+    });
 
-    await googleScreen.closeBrowser();
+    it('Should run GET request to WeatherAPI to obtain the temperature', async () =>{
+        const apiTem = await OpenweathermapAPI.getTempfromAPI();
+        console.log("API Temp:", apiTem);
+    });
 
-    const apiTem = await OpenweathermapAPI.getTempfromAPI();
-    console.log("API Temp:", apiTem);
+    it('Should compare the two temperatures', async () =>{
+        const tempDifference = parseFloat(googleTemp) - apiTem;
+        console.log("Temperature difference:", tempDifference);
+    })
 
-    const tempDifference = parseFloat(googleTemp) - apiTem;
-    console.log("Temperature difference:", tempDifference);
-}
 
-compareTemperatureTest();
+
+
+})
+
+// async function compareTemperatureTest() {
+//    //navigate the app
+//     await GoogleSearchPage.goToURL();
+
+//     //add a todo
+//     await GoogleSearchPage.runSearch("Weather in San Francisco, California");
+
+//     var title = await driver.getTitle();
+//     console.log('Title is:',title);
+
+//     const googleTemp = await GoogleSearchPage.getTemp();
+//     console.log('Google Temperature:', googleTemp);
+
+//     await googleScreen.closeBrowser();
+
+//     const apiTem = await OpenweathermapAPI.getTempfromAPI();
+//     console.log("API Temp:", apiTem);
+
+//     const tempDifference = parseFloat(googleTemp) - apiTem;
+//     console.log("Temperature difference:", tempDifference);
+// }
+
+// compareTemperatureTest();
